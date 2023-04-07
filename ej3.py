807bf37dc21c33c00f6bb3911e05145d50622e66
julian1vidal <63807211+julian1vidal@users.noreply.github.com>
2023-04-07 19:17:31 -0300

    ej3 con matriz tridiag aleatoria, agregue excepciones al dividir por cero

--- ej3.py
+++ ej3.py
@@ -1,11 +1,24 @@
 import numpy as np
 
-arr = np.array([[1, 0, 0, 0, 0], [3, 2, 3, 0, 0], [0, 4, 1, 1, 0], [0, 0, 1, 3, 1], [0, 0, 0, 2, 1]], dtype=float)
-x_ini = np.array([1, 1, -1, 0, 5], dtype=float)
-b_ini = np.matmul(arr, x_ini)
+MAX_NUM = 10
+SIZE = 5
 
-# print(np.linalg.solve(arr, b_ini))
-# print(np.matmul(arr, x_ini))
+# Testeo con matrices tridiagonales aleatorias (enteros del 0 al 9 incl.) y de tamano 5x5.
+c_ini = np.random.randint(MAX_NUM, size=SIZE)
+c_ini[SIZE-1] = 0       # cn = 0
+b_ini = np.random.randint(MAX_NUM, size=SIZE)
+a_ini = np.random.randint(MAX_NUM, size=SIZE)
+a_ini[0] = 0            # a1 = 0
+
+# Paso los vectores a matriz para formar a A
+diag_sup = np.diagflat(c_ini[:SIZE-1], 1)   # no cuento a cn
+diag_principal = np.diag(b_ini)
+diag_inf = np.diagflat(a_ini[1:], -1)       # no cuento a a1
+A_mat = diag_sup + diag_principal + diag_inf
+
+# Calculo el termino independiente con un x aleatorio (con el que luego comparo)
+x_ini = np.random.randint(MAX_NUM, size=SIZE)
+d_ini = np.matmul(A_mat, x_ini)
 
 """
 A = [b1   c1   0      ...    0]
@@ -18,12 +31,11 @@ A = [b1   c1   0      ...    0]
 Ax = vec_d
 """
 
-SIZE = b_ini.size
-c_vec = ((np.array([0, 3, 1, 1, 0], dtype=float)).tolist())
-b_vec = ((np.array([1, 2, 1, 3, 1], dtype=float)).tolist())
-a_vec = ((np.array([0, 3, 4, 1, 2], dtype=float)).tolist())
-d_vec = (b_ini.tolist())
-x_vec = ((np.zeros(5, dtype=float)).tolist())
+c_vec = c_ini.tolist()
+b_vec = b_ini.tolist()
+a_vec = a_ini.tolist()
+d_vec = d_ini.tolist()
+x_vec = ((np.zeros(SIZE, dtype=float)).tolist())
 
 
 def resolver_tridiagonal(a_vec, b_vec, c_vec, d_vec):
@@ -32,9 +44,14 @@ def resolver_tridiagonal(a_vec, b_vec, c_vec, d_vec):
     resolver(b_vec, c_vec, d_vec)
 
 
+
 def triangular(a_vec, b_vec, c_vec):  # Triangulo A (sin modificar el vector indep.)
     for i in range(0, SIZE - 1):
-        a_vec[i + 1] /= b_vec[i]  # En a_vec[i+1] guardo los mi+1 (ya que a termina siendo el vector nulo)
+        try:
+            a_vec[i + 1] /= b_vec[i]  # En a_vec[i+1] guardo los mi+1 (ya que a termina siendo el vector nulo)
+        except ZeroDivisionError:
+            print("Error: division por cero")
+            raise SystemExit
         b_vec[i + 1] -= c_vec[i] * a_vec[i + 1]
 
 
@@ -44,12 +61,22 @@ def modificar_indep(a_vec, d_vec):  # Modifico d con los mi guardados en el vect
 
 
 def resolver(b_vec, c_vec, d_vec):
-    x_vec[SIZE - 1] = d_vec[SIZE - 1] / b_vec[SIZE - 1]  # Resuelvo primer caso (bn * xn = dn)
+    try:
+        x_vec[SIZE - 1] = d_vec[SIZE - 1] / b_vec[SIZE - 1]  # Resuelvo primer caso (bn * xn = dn)
+    except ZeroDivisionError:
+        print("Error: division por cero")
+        raise SystemExit
+
     for i in range(SIZE - 2, -1, -1):  # Resuelvo para el resto de la matriz
         termino_2 = c_vec[i] * x_vec[i + 1]
-        x_vec[i] = (d_vec[i] - termino_2) / b_vec[i]
-
+        try:
+            x_vec[i] = (d_vec[i] - termino_2) / b_vec[i]
+        except ZeroDivisionError:
+            print("Error: division por cero")
+            raise SystemExit
 
 resolver_tridiagonal(a_vec, b_vec, c_vec, d_vec)
+for i in range(SIZE):
+    x_vec[i] = round(x_vec[i])
 print(f"NUESTRA: {x_vec}")
 print(f"esperada: {x_ini}")
