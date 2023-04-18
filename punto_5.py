7faa178780e07b71ccb48bea5128d17ad0d45631
Knuclez <57926276+Knuclez@users.noreply.github.com>
2023-04-18 14:56:31 -0300

    Update punto_5.py
    cambio las cosas para usar el punto de juli

--- punto_5.py
+++ punto_5.py
@@ -1,46 +1,57 @@
 import numpy as np
+EPS = 1.0e-4
+
 
 def armar_sistema_laplaciano(vector_solucion):
-    diag_tr = armar_diagonal_triangulada(len(vector_solucion))
-    v_modif = modificar_vector_solucion(vector_solucion)
-    matriz = armar_matriz_triangulada(diag_tr)
-    print(matriz)
-    #resolver(matriz, v_modif)
+    n = len(vector_solucion)
+    a = np.ones(n)
+    b = np.ones(n) * -2
+    c = np.ones(n)
+    a[0] = 0
+    c[-1] = 0
 
-def armar_diagonal_triangulada(n):
-    a_s = -2
-    a_i = 1
-    res = []
-    for j in range(n):
-        res.append(a_s / a_i)
-        a_s -= 1
-        a_i += 1
-
-    return res
-def modificar_vector_solucion(vector_solu):
-    #decido hacerlo por copia, creo el vector_modif, y le asigno de primer elemento el primero de la solu
-    vector_modif = [vector_solu[0]]
-
-    for j in range(1, len(vector_solu)):
-        #para no dividir 1 sobre algo que tiende a 0, saco a_s y a_i en funcion de n
-        #, que son [a_s=-2-n] y [a_i = n + 1], pero aca estoy usando j =n, entonces
-        #debe ser j-1, ya que necesito tomar los de la fila anterior
-        factor_multiplicador = (j) / (-2-(j-1))
-        vector_modif.append (vector_solu[j] - vector_modif[j-1] * factor_multiplicador)
-
-    return vector_modif
-
-def armar_matriz_triangulada(diagonal_triangulada):
-    matriz_d = np.diagflat(diagonal_triangulada)
-
-    diagonal_unos = np.ones((1, len(diagonal_triangulada) - 1))
-    matriz_unos = np.diagflat(diagonal_unos, 1)
-
-    result = matriz_d + matriz_unos
+    result = resolver_tridiagonal(a, b, c, vector_solucion)
+    
     return result
 
-def resolver():
-    pass
+def armar_lista_a(largo):
+  res = []
+  condicion = math.floor(largo/2) + 1
+  for i in range(largo):
+    if (i==condicion):
+      #se genera error numerico al dividir por numeros pequenios
+      # o multiplicar por numeros grandes (asi q tamo bien)
+      res.append(4/largo)
+    else:
+      res.append(0)
 
-vector_solu = [4, 6 ,2 ,3 ,6 ,7]
-armar_sistema_laplaciano(vector_solu)
+  return(res)
+
+def armar_lista_b(largo):
+  res = []
+  elemento = 4 / (largo ** 2)
+  for i in range(largo):
+    res.append(elemento)
+  
+  return res
+
+
+def armar_lista_c(largo):
+  res = []
+  fijo = 12 / (largo ** 2)
+  for i in range(largo):
+    elemento = (-1 + (2 * i / (largo - 1))) * fijo
+    res.append(elemento)
+
+  return res
+
+
+#a = armar_lista_a(101)
+#resultadoa = armar_sistema_laplaciano(a)
+#b = armar_lista_b(101)
+#resultadob = armar_sistema_laplaciano(b)
+#c = armar_lista_c(101)
+#resultadoc = armar_sistema_laplaciano(c)
+#plt.plot(resultadoa)
+#plt.plot(resultadob)
+#plt.plot(resultadoc)
